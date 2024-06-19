0691ea5812c08433ff488bc7a1354b1f5eed178e
omar-t-ahmed <omartahmed@gmail.com>
2024-06-19 17:36:27 -0400

    add http req

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import express from "express";
+import { PORT } from "./config.js";
 
 const app = express()
+
+app.get('/', (req, res) => {
+    console.log(req)
+    return res.status(234).send('Welcome to your to do list')
+})
+
+app.listen(PORT, () => {
+    console.log(`app is listening to port ${PORT}`);
+})
